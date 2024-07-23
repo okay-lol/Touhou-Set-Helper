1c9c82c242c62b77cae70b93292a4764eecf178d
okay lol <58677633+okay-lol@users.noreply.github.com>
2024-07-22 19:20:29 -0600

    Updating setup.py
    Now, at least in theory, anyone could download it if they can make a python 3.8 environment, download the necessary packages, and run setup,py.

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-from setuptools import setup
+from cx_Freeze import setup, Executable
 
-setup()
+base = None    
+
+executables = [Executable("main.py", base = "Win32GUI")]
+
+include_files = [('sprites', 'sprites')]
+
+packages = ["tkinter", "PIL", "pydub", "pynput", "pyperclip", "threading"]
+
+build_exe_options = {
+    'packages': packages,
+    'include_files': include_files,
+}
+
+setup(
+    name = "<Spellcard Sets>",
+    options = {'build_exe': build_exe_options},
+    version = "0.0.0",
+    description = '',
+    executables = executables
+)
